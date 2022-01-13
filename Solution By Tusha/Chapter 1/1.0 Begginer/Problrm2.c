836ed614c962eda1137e4581f44fb2c90c0b54ad
tusha29 <97684558+tusha29@users.noreply.github.com>
2022-01-14 01:08:59 +0600

    gitigone and 1.0 -> problem 2
    .exe file is in git ignore

---
+++ Solution By Tusha/Chapter 1/1.0 Begginer/Problrm2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    
+    printf("2 + 3 = %d\n", 2 + 3);
+    printf("3 - 2 = %d\n", 3 - 2);
+    printf("2 X 3 = %d\n", 2 * 3);
+    printf("5 / 2 = %d\n", 5 / 2);
+
+    return 0;
+}
